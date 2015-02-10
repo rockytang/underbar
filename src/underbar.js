e57c7229949f288f733855bd178f59e42ac617fe
Rocky Tang <rocky.tang168@gmail.com>
2015-02-09 22:15:03 -0800

    .identity to .reject

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,9 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    var length = array.length;
+    n > length? n = length : n = n; 
+    return n === undefined? array[length-1] : array.slice(length - n, length); 
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +49,13 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)) {
+      for(var i = 0; i < collection.length ; i++) {
+        iterator(collection[i], i, collection);
+      }} else { 
+      for(var prop in collection) {
+        iterator(collection[prop], prop, collection);
+      }}
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,12 +77,23 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var o = [];
+    
+    _.each(collection, function(value){
+      if(test(value)) {o.push(value)}
+    })
+    
+    return o;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    
+    return _.filter(collection, function(value){
+      if(!test(value)) {return value;} 
+    })    
   };
 
   // Produce a duplicate-free version of the array.
